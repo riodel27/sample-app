f73a5a7476ec42bc59619fee11c5299f90ec0f7e
Rio <riopamaong@gmail.com>
2019-03-06 18:49:17 +0800

    Updates

---
+++ precache-manifest.1b17b2ce85aca4ed111141d54b5b881c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3070d0854a9ac2a8b44",
+    "url": "/sample-app/static/css/main.df8a0a87.chunk.css"
+  },
+  {
+    "revision": "d3070d0854a9ac2a8b44",
+    "url": "/sample-app/static/js/main.d3070d08.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/sample-app/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "564060594340135fede4",
+    "url": "/sample-app/static/js/runtime~main.56406059.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "629270e46c94b84982a455e97c6f16a4",
+    "url": "/sample-app/index.html"
+  }
+];
